06c1139e4df44a80cdf787c028bb198cfc93b4a7
dilip baghel <dilip.18baghel@gmail.com>
2024-10-25 14:42:12 +0530

    completed the telephone formatter

--- Telephone formatter/script.js
+++ Telephone formatter/script.js
@@ -1,10 +1,23 @@
 const input = document.querySelector("#teleInput");
 const content = document.querySelector("#content");
 
+let prevVal = "";
 input.addEventListener("input", (e) => {
   let inputVal = e.target.value;
 
-  if (true) {
-    content.innerText = inputVal;
+  if (/\d+$/g.test(inputVal)) {
+    input.value = inputVal;
+  } else {
+    input.value = inputVal.substring(0, inputVal.length - 1);
   }
+
+  if (inputVal.length === 4 && prevVal.length < inputVal.length) {
+    input.value = `+(${inputVal.substring(0, 3)}) - ${
+      inputVal[inputVal.length - 1]
+    }`;
+  } else if (inputVal.length === 9 && prevVal.length > inputVal.length) {
+    input.value = inputVal.substring(2, 5);
+  }
+
+  prevVal = inputVal;
 });
